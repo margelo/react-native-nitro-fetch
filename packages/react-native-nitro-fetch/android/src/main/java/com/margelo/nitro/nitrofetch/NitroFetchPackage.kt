25e8d8fecec7cdded3cbf01508936f5e560917af
Ritesh Shukla <riteshshukla2381@gmail.com>
2026-04-07 11:48:36 +0530

    Update Nitro Fetch

--- packages/react-native-nitro-fetch/android/src/main/java/com/margelo/nitro/nitrofetch/NitroFetchPackage.kt
+++ packages/react-native-nitro-fetch/android/src/main/java/com/margelo/nitro/nitrofetch/NitroFetchPackage.kt
@@ -1,11 +1,11 @@
 package com.margelo.nitro.nitrofetch
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfoProvider
 
-class NitroFetchPackage : TurboReactPackage() {
+class NitroFetchPackage : BaseReactPackage() {
     override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
         return null
     }
