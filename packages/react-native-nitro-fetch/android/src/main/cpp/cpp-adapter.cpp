ea2d0657d13ee7aba6ad42cc513e0f66112688d4
riteshshukla04 <riteshshukla2381@gmail.com>
2026-07-08 17:02:29 +0530

    chore: intialise-> RegisterNatives

--- packages/react-native-nitro-fetch/android/src/main/cpp/cpp-adapter.cpp
+++ packages/react-native-nitro-fetch/android/src/main/cpp/cpp-adapter.cpp
@@ -1,6 +1,9 @@
-#include <jni.h>
 #include "nitrofetchOnLoad.hpp"
+#include <jni.h>
+#include <fbjni/fbjni.h>
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return margelo::nitro::nitrofetch::initialize(vm);
+  return facebook::jni::initialize(vm, []() {
+    margelo::nitro::nitrofetchwebsockets::registerAllNatives();
+  });
 }
